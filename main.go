4e407793c15a23e3e002d334efd8fb7473bec999
svetlana-rezvaya <svetlana-2305@mail.ru>
2020-10-21 20:39:33 +0300

    Add loop

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import "fmt"
 
 func main() {
 	n := 23
-	if n%2 == 1 {
-		n = n*3 + 1
-	} else {
-		n = n / 2
-	}
+	for {
+		if n%2 == 1 {
+			n = n*3 + 1
+		} else {
+			n = n / 2
+		}
 
-	fmt.Println(n)
+		fmt.Println(n)
+	}
 }
